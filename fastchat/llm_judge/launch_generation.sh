1ef012488c2657684f939324620105ac3aa28269
kirill.fedyanin <kirill.fedyanin@gmail.com>
2024-02-06 12:11:41 +0400

    Generations fixed

--- fastchat/llm_judge/launch_generation.sh
+++ fastchat/llm_judge/launch_generation.sh
@@ -3,6 +3,14 @@ python gen_model_answer.py --model-path=TII-Noor-Internal/Noor-DPO-006-Two-Epoch
 python gen_model_answer.py --model-path=fedyanin/Noor-Chat-Mix-Fixed --model-id=noor-chat-mix --num-gpus-total=4
 python gen_model_answer.py --model-path=TII-Noor-Internal/Noor-7B-Pretrained --model-id=noor-pretrained --num-gpus-total=4
 python gen_model_answer.py --model-path=TII-Noor-Internal/Noor-7B-Chat --model-id=noor-chat --num-gpus-total=4
+#
+#
+#python gen_judgment.py --model-list noor-005 noor-006 noor-chat-mix noor-chat noor-pretrained
 
 
-python gen_judgment.py --model-list noor-005 noor-006 noor-chat-mix noor-chat noor-pretrained
+#python gen_model_answer.py --model-path=fedyanin/Noor-DPO-008 --model-id=noor-008 --num-gpus-total=4
+#python gen_model_answer.py --model-path=fedyanin/Noor-DPO-009 --model-id=noor-009 --num-gpus-total=4
+python gen_model_answer.py --model-path=fedyanin/Noor-DPO-010 --model-id=noor-010 --num-gpus-total=4
+#python gen_model_answer.py --model-path=fedyanin/Noor-DPO-011 --model-id=noor-011 --num-gpus-total=4
+python gen_judgment.py --model-list noor-005 noor-006 noor-chat-mix noor-pretrained noor-chat noor-010 --parallel 2
+
--- fastchat/model/model_adapter.py
+++ fastchat/model/model_adapter.py
@@ -1408,6 +1408,28 @@ class NoorChatAdapter(BaseModelAdapter):
         return get_conv_template("noor-chat")
 
 
+
+class JaisChatAdapter(BaseModelAdapter):
+    def match(self, model_path: str):
+        return "jais" in model_path.lower()
+
+    def load_model(self, model_path, from_pretrained_kwargs):
+        tokenizer = AutoTokenizer.from_pretrained(model_path)
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+        model = AutoModelForCausalLM.from_pretrained(
+            model_path,
+            trust_remote_code=True,
+            **from_pretrained_kwargs,
+        )
+
+        model.config.eos_token_id = tokenizer.eos_token_id
+        model.config.pad_token_id = tokenizer.pad_token_id
+        return model, tokenizer
+
+    def get_default_conv_template(self, model_path: str) -> Conversation:
+        return get_conv_template("jais-chat")
+
+
 class FalconChatAdapter(BaseModelAdapter):
     def match(self, model_path: str):
         return "falcon" in model_path.lower() and "chat" in model_path.lower()
@@ -2475,6 +2497,7 @@ register_model_adapter(ChangGPTAdapter)
 register_model_adapter(TuluAdapter)
 register_model_adapter(FalconChatAdapter)
 register_model_adapter(NoorChatAdapter)
+register_model_adapter(JaisChatAdapter)
 register_model_adapter(FalconAdapter)
 register_model_adapter(TigerBotAdapter)
 register_model_adapter(BaichuanAdapter)
